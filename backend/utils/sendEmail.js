9b59c68c46a0ac9cfd4b07c3cfef9291ec4a1b2b
yogita <yogita631@gmail.com>
2022-02-10 02:41:43 +0530

    fix nodemailer bugs

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,7 +2,9 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async options => {
     const transporter = nodemailer.createTransport({
-        service: "Gmail",
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USERNAME,
             pass: process.env.EMAIL_PASSWORD
